6bd034a65b0137d8e4a948f8653b5c4ec351df3f
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-02-02 02:55:57 +0900

    add: activities, people, project constant

---
+++ pages/constants/project.ts
@@ -0,0 +1,28 @@
+import { SLIDE_ITEM } from "pages/types/people/slide-item";
+import bettle from "public/images/project/project-bettle.png";
+import catNeighbor from "public/images/project/project-cat-neighbor.png";
+import jaeumi from "public/images/project/project-jaeumi.png";
+import kuMap from "public/images/project/project-ku-map.png";
+import naviya from "public/images/project/project-naviya.png";
+import nowWhat from "public/images/project/project-now-what.png";
+import sent from "public/images/project/project-sent.png";
+import solitaryDiningRoom from "public/images/project/project-solitary-dining-room.png";
+import spaceDaily from "public/images/project/project-space-daily.png";
+import tabam from "public/images/project/project-tabam.png";
+import timeBank from "public/images/project/project-time-bank.png";
+import toTomorrow from "public/images/project/project-to-tomorrow.png";
+
+export const ProjectItem: SLIDE_ITEM[] = [
+  { name: "bettle", src: bettle },
+  { name: "catNeighbor", src: catNeighbor },
+  { name: "jaeumi", src: jaeumi },
+  { name: "kuMap", src: kuMap },
+  { name: "naviya", src: naviya },
+  { name: "nowWhat", src: nowWhat },
+  { name: "sent", src: sent },
+  { name: "solitaryDiningRoom", src: solitaryDiningRoom },
+  { name: "spaceDaily", src: spaceDaily },
+  { name: "tabam", src: tabam },
+  { name: "timeBank", src: timeBank },
+  { name: "toTomorrow", src: toTomorrow },
+];
