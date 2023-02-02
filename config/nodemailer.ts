b65ef61ff309df2acef688bca8a71e426db36f94
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2023-02-03 02:56:05 +0900

    join us 완성

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,8 +1,8 @@
 // 이 파일은 사용하지 않습니다 We don't use this file
 import nodemailer from "nodemailer";
 
-const email = "test@example.com";
-const pass = "구글 앱 비밀번호";
+const email = process.env.NODEMAILOR_EMAIL;
+const pass = process.env.NODEMAILOR_PASSWORD;
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -9,6 +9,10 @@ import { FireLottie } from "pages/components/lottie/lottie";
 import RocketImg from "public/assets/Rocket.png";
 import { motion } from "framer-motion";
 import S3upload from "pages/components/s3upload/index";
+import { useRecoilState } from "recoil";
+import { joinModalOpen, isLaunched } from "pages/constants/atoms";
+import Text from "public/assets/homeMainSmallLogo.png";
+import Logo from "public/assets/logo.png";
 const variants = {
   open: { opacity: 1 },
   closed: { opacity: 0 },
@@ -16,8 +20,9 @@ const variants = {
 
 export default function Join() {
   const [loading, setLoading] = useState(true);
-  const [launch, setLaunch] = useState(false);
-  const [infoOpen, setInfoOpen] = useState(false);
+  const [launch, setLaunch] = useRecoilState(isLaunched);
+  const [modalPage, setModalPage] = useState(1);
+  const [modalOpen, setModalOpen] = useRecoilState(joinModalOpen);
   const isDesktop = useMediaQuery({ minDeviceWidth: 1000 });
   const isMobile = useMediaQuery({ maxWidth: 1000 });
   const config = {
@@ -37,6 +42,17 @@ export default function Join() {
     console.log("loaded");
     setLoading(false);
   }, [isMobile, isDesktop]);
+  useEffect(() => {
+    return () => {
+      setLaunch(false);
+      setModalOpen(false);
+    };
+  }, []);
+  useEffect(() => {
+    return () => {
+      setModalPage(1);
+    };
+  }, [modalOpen]);
   return (
     <div>
       <Head>
@@ -46,48 +62,97 @@ export default function Join() {
       </Head>
       {!loading && (
         <S.Container>
-          <motion.nav
-            animate={infoOpen ? "open" : "closed"}
+          {!launch && (
+            <S.TitleWrapper isMobile={isMobile}>
+              <img src={Text.src} />
+              <img src={Logo.src} />
+              <p>11기 모집</p>
+            </S.TitleWrapper>
+          )}
+          {launch && (
+            <S.TitleWrapper isMobile={isMobile}>
+              <p>지원해주셔서 감사합니다</p>
+              <br />
+              <p style={{ fontSize: "1.8rem" }}>
+                NEXT에서 세상을 바꿔나갈 여러분의 도전을 응원합니다!
+              </p>
+            </S.TitleWrapper>
+          )}
+          <motion.div
+            animate={modalOpen ? "open" : "closed"}
             variants={variants}
-            style={{ zIndex: "80" }}
+            style={{ zIndex: "10", opacity: "0" }}
           >
-            <S.ModalContainer infoOpen={infoOpen}>
-              <S3upload />
-              {/* 1. 지원방식 <br />
-              <br />
-              <p>
-                a. 아래 [지원서 다운로드] 버튼을 클릭한 후 지원서 양식 다운로드{" "}
-                <br />
-                b. 지원기간(2월 26일 오전 0시 ~ 3월 3일 오후 5시) 내에 지원서
-                작성 <br />
-                c. 아래 [지원하기] 버튼을 클릭하여 구글폼 작성 및 지원서 업로드
-                *이[지원하기]버튼은 지원 기간 중에만 확인 가능합니다.
-                <br /> d. 각 문항별 글자수 : 1000자 이내(공백 포함) <br />
-                e. 제출한 지원서는 수정할 수 없습니다.
-                <br /> f. 세종캠퍼스 학생은 지원이 불가합니다. (세종캠퍼스
-                멋쟁이사자처럼 지원 가능)
-                <br />
-              </p>
-              <br /> 2. 학회 운영비 관련 보증금 제도 안내
-              <br />
-              <br />
-              <p>
-                현 코로나 상황을 고려하여 NEXT x LIKELION은 원활한 학회 운영을
-                위해 보증금 제도를 운영하고 있습니다. 새로 들어오시는 학회원들은
-                10만원의 보증금을 납부하고, 해당 보증금은 해커톤 행사. 세션 장소
-                대관 및 기타 학회 운영비용으로 사용될 예정입니다. 학회원들은
-                모든 회계 정산 내용을 구글 드라이브에서 확인하실 수 있으며
-                활동이 끝난 후 남은 금액을 1/n 하여 전액 반환해 드립니다.
-              </p>
-              <br /> */}
+            <S.ModalContainer infoOpen={modalOpen}>
+              <S.CloseBtnWrapper>
+                <div onClick={() => setModalOpen(false)}>X</div>
+              </S.CloseBtnWrapper>
+              <S.ModalContentWrapper>
+                {modalPage == 1 && (
+                  <S.InfoModal>
+                    <h2>11기 지원 안내</h2>
+                    <br />
+                    <p>
+                      <span>1. 지원방식</span>
+                      <br /> a. 아래 <mark>[지원서 다운로드]</mark> 버튼을
+                      클릭한 후 지원서 양식 다운로드 <br />
+                      b. 지원기간<b>(2월 6일 오전 0시 ~ 2월 15일 오후 5시)</b>
+                      내에 지원서 작성 <br />
+                      c. 아래 <mark>[지원하기]</mark> 버튼을 클릭하여 지원서
+                      업로드
+                      <br /> <b>**</b>[지원하기] 버튼은 지원 기간 중에만 확인
+                      가능합니다.
+                      <br /> d. 각 문항별 글자수 : <b>1000자 이내</b>(공백 포함){" "}
+                      <br />
+                      e. 제출한 지원서는 수정할 수 없습니다.
+                      <br /> f. 세종캠퍼스 학생은 지원이 불가합니다. (세종캠퍼스
+                      멋쟁이사자처럼 지원 가능) <br />
+                      <br />
+                      <span>2. 면접 촬영 및 개인정보 수집 안내</span>
+                      <br /> 면접 평가는 모두 대면으로 이뤄집니다. <br /> 공정한
+                      면접 평가를 위해 면접 내용을 촬영 및 수집할 예정입니다.
+                      <br />
+                      촬영한 면접영상 및 개인정보는 선발과정에서만 활용되며,
+                      리크루팅 이후 즉시 폐기될 예정입니다. <br /> <br />
+                      <span>3. 학회 보증금 제도 안내</span> <br />
+                      원활한 학회 운영을 위해 보증금 제도를 운영하고 있습니다.
+                      <br />
+                      새로 들어오시는 학회원들은 <b>10만원</b>의 보증금을
+                      납부하고, 해당 보증금은 학회 운영비용으로만 사용될
+                      예정입니다.
+                      <br />
+                      학회원들은 모든 회계 정산 내용을 구글 드라이브에서
+                      확인하실 수 있으며 활동이 끝난 후 남은 금액을 1/n 하여
+                      전액 반환해 드립니다. <br />
+                      <br />
+                      <span>4. 오리엔테이션 필참</span>
+                      <br />
+                      최종 합격 이후 <b>3월 5일</b>에 진행되는 OT는 필수
+                      참여입니다. 원활한 학회 운영을 위해, OT를 고려하여 개인
+                      일정을 조정해주시면 감사하겠습니다
+                    </p>
+                    <S.NextBtnWrapper isMobile={isMobile}>
+                      <button
+                        onClick={() =>
+                          (location.href =
+                            "https://next-recruit.s3.ap-northeast-2.amazonaws.com/assets/NEXT_11%EA%B8%B0_%EC%A7%80%EC%9B%90%EC%84%9C_%EC%96%91%EC%8B%9D.docx")
+                        }
+                      >
+                        지원서 다운로드
+                      </button>
+                      <button onClick={() => setModalPage(2)}>지원하기</button>
+                    </S.NextBtnWrapper>
+                  </S.InfoModal>
+                )}
+                {modalPage == 2 && <S3upload />}
+              </S.ModalContentWrapper>
             </S.ModalContainer>
-          </motion.nav>
+          </motion.div>
           <S.SpaceContainer isMobile={isMobile}>
             <S.RocketContainer
               onClick={() => {
-                setInfoOpen((infoOpen) => !infoOpen);
-                // setLaunch(true);
-                console.log(infoOpen);
+                setModalOpen((modalOpen) => !modalOpen);
+                console.log(modalOpen);
               }}
               launched={launch}
             >
@@ -105,6 +170,11 @@ export default function Join() {
                 src="https://media-public.canva.com/Y2Ods/MAD9dPY2Ods/1/tl.png"
               />
             </S.Planet>
+            {!modalOpen && !launch && (
+              <S.RocketInfo>
+                <p>로켓을 클릭해보세요!</p>
+              </S.RocketInfo>
+            )}
           </S.SpaceContainer>
 
           <ReactPannellum
